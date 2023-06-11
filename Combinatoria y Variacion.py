22e8c96f93b0366b6be4087e6c8aca679bcc8002
Daniel <vcdanielj@gmail.com>
2023-06-11 17:24:48 -0400

    M
    Modulo Ejercicio 4a Agregado

--- Combinatoria y Variacion.py
+++ Combinatoria y Variacion.py
@@ -46,7 +46,28 @@ def ejercicio_3():
     pass
 
 def ejercicio_4():
-    pass
+    print("""Una Moneda es Lanzada 10 veces
+¿Cuantos Resultados Tienen Exactamente 3 Caras?""")
+    n = 10
+    r = 3
+
+    nCr = factorial(n)/(factorial(r)*factorial(n-r))
+
+    print(f"Existen un total de {nCr} resultados que tienen exactamente 3 caras")
+    x = input()
+    print("¿Cuantos Resultados Tienen a lo Sumo 3 Caras?")
+
+    r = 3
+
+    nCr = 0
+
+    for i in range(0, r+1):
+        nCr += factorial(n)/(factorial(i)*factorial(n-i))
+    
+    print(f"Existen un total de {nCr} resultados que tienen como máximo 3 caras")
+    x = input()
+
+    ejercicios_4_5()
 
 def ejercicio_5():
     pass
@@ -85,5 +106,13 @@ def ejercicios_4_5():
             ejercicio_5()
         case 3:
             menu_inicial()
+
+def factorial(n):
+    fact = 1
+    for i in range(1, n+1):
+        fact *= i
+    
+    return fact
+
 
 menu_inicial()
